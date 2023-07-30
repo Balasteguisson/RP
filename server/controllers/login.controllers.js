200900b8c5d38ec1f550cf86977ee1bdaece8ac6
Guillermo BG <99534498+Balasteguisson@users.noreply.github.com>
2023-07-30 11:45:37 +0200

    cambios en login

--- server/controllers/login.controllers.js
+++ server/controllers/login.controllers.js
@@ -10,9 +10,12 @@ export const signUpUser = async (req, res) => {
     let queryNewUser = `INSERT INTO usuarios (EMAIL,CLAVE) VALUES (?,?) `;
 
     let result = await pool.execute(queryNewUser, [user, password], (err, results, fields) => {
-
+        if (err)
+            res.status(400).json('No se ha podido registrar al usuario.')
     })
 
+    console.log("HOLA")
     console.log(result);
+    console.log("HOLA")
     res.status(200).json({ message: 'Usuario creado' })
 }
