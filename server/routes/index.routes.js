bee4c8276bd45db3ae18cfb9e161bd07834adb96
Guillermo BG <99534498+Balasteguisson@users.noreply.github.com>
2023-07-30 11:24:17 +0200

    cambios en llamada a base de datos

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -4,9 +4,14 @@ import { pool } from '../db.js';
 const router = Router();
 
 router.get('/ping', async (req, res) => {
-    const [rows, fields] = await pool.query('SELECT 1+ 1 as result');
-    console.log(rows[0]);
-    res.json('ping');
+    try {
+        const [rows, fields] = await pool.query('SELECT 1+ 1 as result');
+        console.log(rows[0]);
+        res.json('Base de datos funcionando');
+    } catch {
+        console.log('BBDD apagada')
+        res.json('Base de datos apagada')
+    }
 })
 
 export default router;
