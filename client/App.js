73e067f58a2a5d978a045a27296cae2a77dc52f9
Balasteguisson <gbg.balas@gmail.com>
2023-08-07 22:06:20 +0200

    Empiezo a crear la pantalla de login

--- client/App.js
+++ client/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export default function App() {
+function LoginScreen() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, paddingTop: insets.top }}>
+      <Text>Aquí va el formulario de login</Text>
+      <Text>Aquí va el boton de login</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+
+  return (
+    <SafeAreaProvider>
+      <LoginScreen></LoginScreen>
+    </SafeAreaProvider>
+
+  );
+}
