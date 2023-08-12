00d11918a4efb1b235925740109692dc65dd4be8
Balasteguisson <gbg.balas@gmail.com>
2023-08-12 12:25:08 +0200

    Estilizo ConstantList y Constant Items

--- client/src/components/ConstantItem.jsx
+++ client/src/components/ConstantItem.jsx
@@ -4,11 +4,12 @@ import { View, Text, StyleSheet } from 'react-native'
 const ConstantItem = (props) => {
   return (
     <View key={props.id} style={styles.container}>
-      <Text>{props.id}</Text>
-      <Text>{props.date}</Text>
+      <Text style={styles.constTitle}>{props.id}</Text>
+      <Text style={styles.consDate}>{props.date}</Text>
       <Text>
         {props.value[0]} {props.value[1]} {props.unit}
       </Text>
+      <Text>Más</Text>
     </View>
   )
 }
@@ -17,7 +18,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'lightblue',
     width: '100%'
-  }
+  },
+  constTitle: {
+    fontWeight: '600',
+    fontSize: 20
+  },
+  consDate: {}
 })
 
 export default ConstantItem
